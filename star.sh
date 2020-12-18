18d5d797d9e59a583aec0d4a7e21d0a574718648
evbauer <18405113+evbauer@users.noreply.github.com>
2020-12-18 18:22:53 -0500

    update module scripts for newer mesa_test

--- star.sh
+++ star.sh
@@ -10,7 +10,7 @@
 clean_caches
 
 if [ -n "${USE_MESA_TEST}" ]; then
-    mesa_test test_one ${MESA_DIR} ${SLURM_ARRAY_TASK_ID} --module=star ${MESA_TEST_OPTIONS}
+    mesa_test test ${SLURM_ARRAY_TASK_ID} --module=star ${MESA_TEST_OPTIONS}
 else
     cd ${MESA_DIR}/star/test_suite
     ./each_test_run ${SLURM_ARRAY_TASK_ID}
