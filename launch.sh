6631a8eaa175ab7f35f4965a99a148f27fa6457a
evbauer <18405113+evbauer@users.noreply.github.com>
2021-06-16 15:44:57 -0400

    add some missing scripts

---
+++ launch.sh
@@ -0,0 +1,13 @@
+
+source .bashrc
+cd mesa-helios-test
+
+env
+
+which ruby
+which mesa_test
+gfortran --version
+
+date --rfc-3339='seconds' | tr -d '\n' >> $HOME/track_share.txt
+sshare | grep ebauer >> $HOME/track_share.txt
+./runMesaTest.sh
