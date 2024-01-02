772c85c6563441076db889614940af1382822fe6
evbauer <18405113+evbauer@users.noreply.github.com>
2024-01-02 10:56:52 -0500

    job tweaks

--- mesa_test.sh
+++ mesa_test.sh
@@ -5,8 +5,7 @@ export MESA_TMP=$CONROY_SCRATCH/ebauer/mesa-git-tests # Where to checkout each M
 export MESA_SCRIPTS=~/mesa-helios-test # Where this script sits
 
 export OMP_NUM_THREADS=8
-export MESASDK_ROOT=$DATA_DIR/mesasdks/mesasdk-20.3.2
-#export MESASDK_ROOT=$CONROY_SCRATCH/ebauer/mesa-git-tests/mesa_test/mesasdk
+export MESASDK_ROOT=$DATA_DIR/mesasdks/mesasdk-23.7.3
 export MESA_CLUSTER=~/mesa-test
 #export GYRE_DIR=$MESA_DIR/gyre/gyre
 source $MESASDK_ROOT/bin/mesasdk_init.sh
